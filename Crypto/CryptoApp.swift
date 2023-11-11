2a43d70fb40916f836ee29e4d8836f4e45578413
Kranti <darkphoton20@gmail.com>
2023-11-11 19:40:43 +0800

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Kranti on 2023/11/11.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
